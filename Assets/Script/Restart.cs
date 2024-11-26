5d959dfe2236bb16925f0a894db7e3683f126ff4
JonayGaroe <jonaygaroe@gmail.com>
2024-11-26 02:42:04 +0100

    Mejorado en casa
    le añadi un par de ui y botones

---
+++ Assets/Script/Restart.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Restart : MonoBehaviour
+
+{
+    [SerializeField]
+    GameObject finalPartida;
+
+     [SerializeField]
+    GameObject empiezaPartida;
+
+    bool estaJugando = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EmpezarJuego()
+    {
+        empiezaPartida.SetActive(false);
+        if (estaJugando == false)
+        {
+            estaJugando = true;
+            empiezaPartida.SetActive(false);
+
+        }
+
+
+    }
+
+
+
+
+    public void ReiniciarJuego()
+    {
+
+        estaJugando = false;
+        SceneManager.LoadScene("otraCosaa");
+        finalPartida.SetActive(false);
+        empiezaPartida.SetActive(true);
+    
+
+    }
+
+
+}
